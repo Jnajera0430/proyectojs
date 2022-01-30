4e2caec4ad7704e32f0092abc9379f451a63de25
Jose Najera <najerajoseantonio052@gmail.com>
2022-01-30 01:15:13 -0500

    actualizando primer taller del curso js

---
+++ figuras.js
@@ -0,0 +1,29 @@
+//Calculos del cuadrado
+console.group("Cuadrado");
+const lcuadrado = 5;
+
+console.log("El lado del cuadrado es de: " + lcuadrado);
+
+const pcuadrado = lcuadrado * 4;
+
+console.log("El perímetro del cuadrado es igual a: " + pcuadrado);
+
+const Acuadrado = lcuadrado * lcuadrado;
+
+console.log("El área del cuadrado es igual a: " + Acuadrado + "cm²");
+console.groupEnd();
+
+//Calculos del trianulo
+console.group("Triangulo");
+const ltriangulo1 = 6;
+const ltriangulo2 = 6;
+const ltriangulo3 = 4;
+
+console.log(
+    "Los lados de un triangulo miden: "
+    + ltriangulo1 + "cm "
+    + ltriangulo2 + "cm "
+    + ltriangulo3 + "cm "
+)
+
+console.groupEnd();
