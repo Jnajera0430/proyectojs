3d1f6603f72baae962470c7fb6a951dfbd8680f9
Jose Najera <najerajoseantonio052@gmail.com>
2022-01-31 01:10:55 -0500

    se encapsulo el codigo para hacerlo mas corto

--- figuras.js
+++ figuras.js
@@ -1,29 +1,33 @@
 //Calculos del cuadrado
 console.group("Cuadrado");
-const lcuadrado = 5;
+//const lcuadrado = 5;
 
-console.log("El lado del cuadrado es de: " + lcuadrado);
+//console.log("El lado del cuadrado es de: " + lcuadrado);
+function pcuadrado(lcuadrado) {
+    return lcuadrado * 4 +" cm";
+} 
 
-const pcuadrado = lcuadrado * 4;
+function Acuadrado(lcuadrado){
+   return lcuadrado * lcuadrado + " cm²";
+}  
 
-console.log("El perímetro del cuadrado es igual a: " + pcuadrado);
-
-const Acuadrado = lcuadrado * lcuadrado;
-
-console.log("El área del cuadrado es igual a: " + Acuadrado + "cm²");
+//console.log("El área del cuadrado es igual a: " + Acuadrado + "cm²");
 console.groupEnd();
 
 //Calculos del trianulo
 console.group("Triangulo");
-const ltriangulo1 = 6;
-const ltriangulo2 = 6;
-const ltriangulo3 = 4;
+//const ltriangulo1 = 6;
+//const ltriangulo2 = 6;
+//const btriangulo = 4;
 
-console.log(
-    "Los lados de un triangulo miden: "
-    + ltriangulo1 + "cm "
-    + ltriangulo2 + "cm "
-    + ltriangulo3 + "cm "
-)
+function ptriangulo(lado1,lado2,base){
+    return lado1 + lado2 + base + " cm";
+}
+
+function Atriangulo(h,base){
+    return (h*base)/2 + " cm²"
+}
+
+//console.log("Los lados de un triangulo miden: "+ ltriangulo1 + "cm "+ ltriangulo2 + "cm "+ btriangulo + "cm ")
 
 console.groupEnd();
